5b361900cb5f0cf531dda884b0c04b481b2d9a12
Abdurahmon55 <116945590+Abdurahmon55@users.noreply.github.com>
2023-04-04 22:29:58 +0500

    menue database  create

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+export const menue=[
+    {
+        name:'Learning', id:1, ul:[{li:'Learn Hub'},{li:'Manuals'},{li:'Tutorials'},{li:'Communities'}]
+    },
+    {
+        name:'Resources', id:2, ul:[{li:'Tutorials'},{li:'Editorials'},{li:'Product '},{li:'Newsroom'}]
+    },
+    {
+        name:'About', id:3, ul:[{li:'Company'},{li:'Careers '},{li:'Legal'},{li:'Help'}]
+    },
+    {
+        name:'Product', id:4, ul:[{li:'Business'},{li:'Designers'},{li:'Classrooms'},{li:'Newcomers'}]
+    },
+]
