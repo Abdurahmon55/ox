6b90e7ccbc8b4f852a6c8b675b2a4fbe4aa11f15
Abdurahmon55 <116945590+Abdurahmon55@users.noreply.github.com>
2023-04-05 05:57:04 +0500

    footer text

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Button from '../../ui/Button'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+        <div className='text-center mt-36 border py-20 rounded-lg  bg-[#FF5A09]'>
+            <h2 className='w-[768px] m-auto text-white'>Get ahead of the competition with Experts</h2>
+            <button className='bg-white button'>Discover Resources</button>
+        </div>
+    </div>
   )
 }
 
